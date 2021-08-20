9446edb776a1a425b09bb83af648d4c3972b42dd
Benjamin Winkler <benjiwinkler@yahoo.com>
2021-08-20 18:13:09 -0500

    added base setup.

---
+++ client/src/components/imageUploader/index.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+const { cloudinary } = require('../../utils/cloudinary');
+
+export default function Upload() {
+    const [fileInputState, setFileInputState] = useState('');
+    const [previewSource, setPreviewSource] = useState('');
+    const [selectedFile, setSelectedFile] = useState();
+    const [successMsg, setSuccessMsg] = useState('');
+    const [errMsg, setErrMsg] = useState('');
+    const handleFileInputChange = (e) => {
+        const file = e.target.files[0];
+        previewFile(file);
+        setSelectedFile(file);
+        setFileInputState(e.target.value);
+    };
+
+    const previewFile = (file) => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onloadend = () => {
+            setPreviewSource(reader.result);
+        };
+    };
+
+    const handleSubmitFile = (e) => {
+        e.preventDefault();
+        if (!selectedFile) return;
+        const reader = new FileReader();
+        reader.readAsDataURL(selectedFile);
+        reader.onloadend = () => {
+            uploadImage(reader.result);
+        };
+        reader.onerror = () => {
+            console.error('Unable to Read image.');
+            setErrMsg('something went wrong!');
+        };
+    };
+
+    const uploadImage = async (base64EncodedImage) => {
+        try {
+            const image = JSON.stringify(base64EncodedImage);
+            const uploadResponse = await cloudinary.uploader.upload(image, {
+                upload_preset: 'dev_setups',
+            });
+            console.log(uploadResponse);
+            setFileInputState('');
+            setPreviewSource('');
+            setSuccessMsg('Image uploaded successfully');
+        } catch (err) {
+            console.error(err);
+            setErrMsg('Something went wrong!');
+        }
+    };
+    return (
+        <div>
+            <h1 className="title">Upload an Image</h1>
+            <form onSubmit={handleSubmitFile} className="form">
+                <input
+                    id="fileInput"
+                    type="file"
+                    name="image"
+                    onChange={handleFileInputChange}
+                    value={fileInputState}
+                    className="form-input"
+                />
+                <button className="btn" type="submit">
+                    Submit
+                </button>
+            </form>
+            {previewSource && (
+                <img
+                    src={previewSource}
+                    alt="chosen"
+                    style={{ height: '300px' }}
+                />
+            )}
+        </div>
+    );
+}

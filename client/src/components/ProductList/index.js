418209c7590b90a117b5943d6f91b62124de9b4a
Hugh Bowie IV <hughbowie@me.com>
2021-08-23 19:22:34 -0400

    Update index.js

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -1,22 +1,18 @@
-
 import React, { useEffect } from 'react';
-import { useQuery } from '@apollo/react-hooks';
-
 import ProductItem from "../ProductItem";
+import { useStoreContext } from '../../utils/GlobalState';// --------- non redux
+//import { useSelector, useDispatch } from "react-redux";// ----------- redux
+import { UPDATE_PRODUCTS } from '../../utils/actions';
+import { useQuery } from '@apollo/react-hooks';
 import { QUERY_PRODUCTS } from "../../utils/queries";
+import { idbPromise } from "../../utils/helpers";
 import spinner from "../../assets/spinner.gif"
 
-// import { useStoreContext } from '../../utils/GlobalState';
-import { UPDATE_PRODUCTS } from '../../utils/actions';
-
-import { idbPromise } from "../../utils/helpers";
-import { useSelector, useDispatch } from "react-redux";
 
 function ProductList() {
-    // const [state, dispatch] = useStoreContext();
-    const state = useSelector(state => state);
-
-    const dispatch = useDispatch();
+    //const state = useSelector(state => state);// --------- redux
+    //const dispatch = useDispatch();// redux
+    const [state, dispatch] = useStoreContext();// -------- non redux
 
     const { currentCategory } = state;
 

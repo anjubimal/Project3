f3a2cbd72c40d8812fc8aa398eb567868fe2644f
Chris Winters <79880339+cwinters87@users.noreply.github.com>
2021-08-23 09:24:49 -0500

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,48 +1,58 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import ImageUpload from "./components/imageUploader";
 
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Nav from './components/Nav';
-import { StoreProvider } from "./utils/GlobalState";
+import Navigation from './components/Nav/index'
+import Footer from './components/Footer/index'
 
 import {
-    ApolloProvider,
-    ApolloClient,
-    InMemoryCache,
-    createHttpLink
+  ApolloProvider,
+  ApolloClient,
+  InMemoryCache,
+  createHttpLink
 } from '@apollo/client';
+
+import { StoreProvider } from "./utils/GlobalState";
+
+import Home from "./pages/Home";
+import Products from "./pages/Products";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Admin from "./pages/Admin";
 
 
 const httpLink = createHttpLink({
-    uri: '/graphql',
+  uri: '/graphql',
 });
 
 const client = new ApolloClient({
-    link: httpLink,
-    cache: new InMemoryCache(),
+  link: httpLink,
+  cache: new InMemoryCache(),
 });
 
 
 function App() {
-    return (
-        <ApolloProvider client={client}>
-            <Router>
-                <div>
-                    <StoreProvider>
-                        <Nav />
-                        <Switch>
-                            <Route exact path="/login" component={Login} />
-                            <Route exact path="/signup" component={Signup} />
-                        </Switch>
-                        <ImageUpload />
-                    </StoreProvider>
-                </div>
-            </Router>
-        </ApolloProvider>
-    );
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div>
+          <StoreProvider>
+            <Navigation />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/products" component={Products} />
+              <Route exact path="/login" component={Login} />
+              <Route exact path="/signup" component={Signup} />
+              <Route exact path="/admin" component={Admin} />              
+            </Switch>
+            <ImageUpload />
+            <Footer />
+          </StoreProvider>
+        </div>
+      </Router>
+    </ApolloProvider>
+
+  );
 }
 
 export default App;

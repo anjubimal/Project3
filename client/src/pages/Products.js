fac61516c2c43d1485e292396346180010a2232c
hugh-bowie <hughbowie@me.com>
2021-08-24 15:15:55 -0400

    added a product to seeds

--- client/src/pages/Products.js
+++ client/src/pages/Products.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import ProductList from '../components/ProductList/index'
+import React from 'react';
+import ProductList from '../components/ProductList';
 
 const Products = () => {
     return (
